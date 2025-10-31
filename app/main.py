2e062f9bd43bee1044c1393a6348b22df87794fe
FabianHeBa <fabianhbc@ciencias.unam.mx>
2025-10-30 22:48:11 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import joblib
 from typing import List
 from decisionTree import SimpleRandomForest
+import os
 
 class PredictionInput(BaseModel):
       features: List[float]
@@ -56,11 +57,14 @@ async def make_predict(input_data: PredictionInput):
             "prediction": predicted_class,
              }
 
-
-
+if __name__ == "__main__":
+     import uvicorn
+     port = int(os.getenv("PORT",8000))
+     uvicorn.run(app, host='0.0.0.0', port=port)
 
 
 
       
+
 
 
